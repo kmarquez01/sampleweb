c6e19d07567512fdb3b499f95e1384d1d0baaec4
kmarquez01 <kirbymarquez15@gmail.com>
2021-09-10 12:47:06 -0700

    update

--- Menu/CardInfo.js
+++ Menu/CardInfo.js
@@ -1,32 +1,38 @@
 const CardInfo = [
     {
         id: 1,
+        
         name: "Yakitori",
         price: 2.00
     },
     {
         id: 2,
+        
         name: "Sushi",
         price: 6.00
     },
     {
         id: 3,
+       
         name: "Udon",
         price: 8.00
     },
     {
         id: 4,
+        
         name: "Ramen",
         price: 7.50
     },
     {
         id: 5,
+        
         name: "Donburi",
         price: 6.50,
 
     },
     {
         id: 6,
+        
         name: "Tonkatsu",
         price: 5.50
     }
